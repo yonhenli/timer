4e4f30cf3f65866e2aa6857ab48a938c73faa054
Yongheng Li <yonhenli@gmail.com>
2020-08-17 09:52:07 -0400

    change script

--- parse.py
+++ parse.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 from scipy import stats
 sns.set(color_codes=True)
 
-filename_host = "d12/1ms/run_1/host.txt"
-filename_did = "d12/1ms/run_1/did.txt"
-filename_vanilla = "d12/1ms/run_1/vanilla.txt"
+filename_host = "d12/run_2/host.txt"
+filename_did = "d12/run_2/did.txt"
+filename_vanilla = "d12/run_2/vanilla.txt"
 
 def parse(filename):
 	i = 0
@@ -31,26 +31,26 @@ host_ltc = np.array(parse(filename_host))
 did_ltc = np.array(parse(filename_did))
 vanilla_ltc = np.array(parse(filename_vanilla))
 
-# pct_10 = np.percentile(arr, 10)
-# pct_25 = np.percentile(arr, 25)
-# pct_50 = np.percentile(arr, 50)
-# pct_75 = np.percentile(arr, 75)
-# pct_99 = np.percentile(arr, 99)
-# min_ = min(latencies)
-# max_ = max(latencies)
+pct_10 = np.percentile(did_ltc, 10)
+pct_25 = np.percentile(did_ltc, 25)
+pct_50 = np.percentile(did_ltc, 50)
+pct_75 = np.percentile(did_ltc, 75)
+pct_99 = np.percentile(did_ltc, 99)
+min_ = min(did_ltc)
+max_ = max(did_ltc)
 
-# print(filename + '\n')
-# print("10 %: " + str(pct_10))
-# print("25 %: " + str(pct_25))
-# print("50 %: " + str(pct_50))
-# print("75 %: " + str(pct_75))
-# print("99 %: " + str(pct_99))
-# print("min: " + str(min_))
-# print("max: " + str(max_))
+# print(host_ltc + '\n')
+print("10 %: " + str(pct_10))
+print("25 %: " + str(pct_25))
+print("50 %: " + str(pct_50))
+print("75 %: " + str(pct_75))
+print("99 %: " + str(pct_99))
+print("min: " + str(min_))
+print("max: " + str(max_))
 
 plt.figure()
 sns.kdeplot(host_ltc,cumulative=True, color="r", label="Host")
 sns.kdeplot(did_ltc,cumulative=True, color="b", label="DID")
 sns.kdeplot(vanilla_ltc,cumulative=True, color="y", label="Vanilla")
-plt.xlabel('ms', fontsize=10)
+plt.xlabel('us', fontsize=10)
 plt.show()
